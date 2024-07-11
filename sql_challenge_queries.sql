41612b380973526b81783df8d543163b833e4291
Jack Thomas <168010304+jackthomas1430@users.noreply.github.com>
2024-07-11 09:23:26 -0600

    0711 updates

--- sql_challenge_queries.sql
+++ sql_challenge_queries.sql
@@ -8,48 +8,22 @@ SELECT * FROM dept_emp;
 -- 1. List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM employees as e
-JOIN salaries s ON 
+JOIN salaries as s ON 
 e.emp_no = s.emp_no
 ORDER BY e.emp_no;
-
--- Create view from query
-CREATE VIEW Employee_info_salary AS
-SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
-FROM employees as e
-JOIN salaries s ON 
-e.emp_no = s.emp_no
-ORDER BY e.emp_no;
-
--- Query the table view created
-SELECT *
-FROM Employee_info_salary;
-
--- Drop view
-DROP VIEW Employee_info_salary;
 
 --2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
---verify results
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE first_name = 'Eran' And last_name = 'Cusworth'
-
 --3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM dept_manager as dm
-JOIN departments d ON 
+JOIN departments as d ON 
 dm.dept_no = d.dept_no
 JOIN employees as e ON 
 dm.emp_no = e.emp_no;
-
---Verify results
-Select *
-From employees 
-where emp_no = '110022';
-
 
 --4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
@@ -93,7 +67,3 @@ FROM employees
 GROUP BY last_name
 ORDER BY frequency DESC;
 
---verify results 
-Select *
-From employees 
-where last_name = 'Baba';
